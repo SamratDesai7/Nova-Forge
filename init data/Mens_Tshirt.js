612dcad919a0ee38afe1a488f8052695e22744a5
Samrat Desai <145000757+SamratDesai7@users.noreply.github.com>
2024-08-30 17:18:11 +0530

    Add files via upload

---
+++ init data/Mens_Tshirt.js
@@ -0,0 +1,38 @@
+[
+    {
+      "product_name": "Classic Crew Neck T-Shirt",
+      "sizes": ["S", "M", "L", "XL"],
+      "price": 1638.18,
+      "description": "Made from 100% soft cotton, this Classic Crew Neck T-Shirt offers a comfortable, casual fit that's perfect for everyday wear. It features a durable ribbed crew neck and double-stitched hems for added durability. Available in a variety of colors to suit your style.",
+      "image_url": "https://assets.ajio.com/medias/sys_master/root/20231011/4R0a/6526b276afa4cf41f53bf59a/-473Wx593H-410394010-jzd-MODEL.jpg"
+    },
+    {
+      "product_name": "Vintage Graphic Tee",
+      "sizes": ["S", "M", "L", "XL"],
+      "price": 1845.00,
+      "description": "Featuring a retro graphic print, this Vintage Graphic Tee is made from a premium cotton blend for a soft, comfortable feel. Its relaxed fit and trendy design make it a must-have for any casual wardrobe.",
+      "image_url": "https://images.lifestyleasia.com/wp-content/uploads/sites/2/2022/01/31163017/Trendy-vintage-graphic-tees-in-Hong-Kong.jpg"
+    },
+    {
+      "product_name": "Performance Athletic Shirt",
+      "sizes": ["S", "M", "L", "XL"],
+      "price": 2458.18,
+      "description": "Crafted with moisture-wicking fabric, the Performance Athletic Shirt is designed for those who push their limits. It offers breathability, flexibility, and a snug fit to keep you cool and comfortable during intense workouts.",
+      "image_url": "https://www.reccy.in/cdn/shop/files/11-05-2023-Michael00384.webp?v=1723865673&width=533"
+    },
+    {
+      "product_name": "Basic V-Neck Tee",
+      "sizes": ["S", "M", "L", "XL"],
+      "price": 1475.82,
+      "description": "This Basic V-Neck Tee is a versatile addition to your wardrobe. Made from soft, breathable fabric, it features a flattering v-neckline and a classic fit, ideal for layering or wearing on its own.",
+      "image_url": "https://shop.mango.com/assets/rcs/pics/static/T5/fotos/S/57010794_99_B.jpg?imwidth=2048&imdensity=1&ts=1681206065859"
+    },
+    {
+      "product_name": "Eco-Friendly Organic Cotton T-Shirt",
+      "sizes": ["S", "M", "L", "XL"],
+      "price": 2041.82,
+      "description": "Sustainably made from 100% organic cotton, this Eco-Friendly T-Shirt is as kind to the environment as it is to your skin. It offers a soft, lightweight feel and a modern, fitted cut, making it a great choice for eco-conscious shoppers.",
+      "image_url": "https://blily.in/wp-content/uploads/2024/07/Organic-Cotton-T-Shirts-1.webp"
+    }
+  ]
+  
